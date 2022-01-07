6f86f56a80538e2a7b4340c70b4a2fdb07b784fd
Basab Dattaray <25799611+basab-dattaray@users.noreply.github.com>
2022-01-07 12:31:59 -0800

    0.0.1: change tsconfig for kooks context

--- examples/hooks-context/src/App.tsx
+++ examples/hooks-context/src/App.tsx
@@ -3,9 +3,9 @@ import { useApplicationContext, ApplicationContextProvider } from "./store";
 const TimerDisplay: React.FunctionComponent = () => {
   const { seconds } = useApplicationContext();
   return (
-    <div className="text-3xl">
-      <span className="mr-5 font-bold">Stopwatch:</span>
-      <span className="font-mono">{seconds.toFixed(1)}</span>
+    <div className='text-3xl'>
+      <span className='mr-5 font-bold'>Stopwatch:</span>
+      <span className='font-mono'>{seconds.toFixed(1)}</span>
     </div>
   );
 };
@@ -13,11 +13,11 @@ const TimerDisplay: React.FunctionComponent = () => {
 const TimerToggle: React.FunctionComponent = () => {
   const { running, onToggle } = useApplicationContext();
   return (
-    <div className="my-5">
+    <div className='my-5'>
+      Hooks Context
       <button
         onClick={onToggle}
-        className="bg-blue-700 text-white px-10 py-2 font-bold rounded-full text-3xl"
-      >
+        className='bg-blue-700 text-white px-10 py-2 font-bold rounded-full text-3xl'>
         {running ? "Stop" : "Start"}
       </button>
     </div>
@@ -28,16 +28,16 @@ const Names: React.FunctionComponent = () => {
   const { names } = useApplicationContext();
   return names ? (
     <>
-      <div className="text-3xl font-bold mb-5">Data</div>
-      <div className="text-3xl font-mono">{JSON.stringify(names)}</div>
+      <div className='text-3xl font-bold mb-5'>Data</div>
+      <div className='text-3xl font-mono'>{JSON.stringify(names)}</div>
     </>
   ) : null;
 };
 
 const App: React.FunctionComponent = () => (
   <ApplicationContextProvider>
-    <div className="mt-10 mx-auto max-w-3xl">
-      <h1 className="font-bold text-5xl mb-5 border-b-2 border-gray-800">
+    <div className='mt-10 mx-auto max-w-3xl'>
+      <h1 className='font-bold text-5xl mb-5 border-b-2 border-gray-800'>
         Hooks - Context
       </h1>
       <TimerDisplay />
